739c959b29e36ab58d9556a5a3caf817ffb0cbea
Nico60 <ndhouailly@gmail.com>
2016-05-29 13:05:37 +0200

    Add interpolators to qs tiles animation [1/2]
    Change-Id: Iaa1923e378e54ea452a2006dcade2bb1cd049d21
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -247,6 +247,7 @@ public class SecureSettings {
         Settings.Secure.TETHERING_ALLOW_VPN_UPSTREAMS,
         Settings.Secure.STATUS_BAR_CLOCK,
         Settings.Secure.QUICK_SETTINGS_TILES_ANIM_STYLE,
-        Settings.Secure.QUICK_SETTINGS_TILES_ANIM_DURATION
+        Settings.Secure.QUICK_SETTINGS_TILES_ANIM_DURATION,
+        Settings.Secure.QUICK_SETTINGS_TILES_ANIM_INTERPOLATOR
     };
 }
