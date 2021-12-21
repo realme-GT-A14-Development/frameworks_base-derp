5d78bf1c004af98a1ffc7fe1a9f4d713f05bda63
SagarMakhar <sagarmakhar@gmail.com>
2021-12-21 15:34:11 +0000

    base: Bring back UDFPS icons [1/3]
    neobuddy89: Adapted for Android 12L
    
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: jhonboy121 <alfredmathew05@gmail.com>
    Co-authored-by: daniml3 <danimoral1001@gmail.com>
    Co-authored-by: DennySPb <dennyspb@gmail.com>
    Co-authored-by: kondors1995 <normandija1945@gmail.com>
    Co-authored-by: jhenrique09 <jhenrique09.mcz@hotmail.com>
    Co-authored-by: Hikari-no-Tenshi <Hikari-no-Tenshi@users.noreply.github.com>
    Co-authored-by: TheScarastic <warabhishek@gmail.com>
    Co-authored-by: nullxception <nullxception@users.noreply.github.com>
    Co-authored-by: Joey Huab <joey@evolution-x.org>
    Co-authored-by: 00day0 <00day0@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7067,6 +7067,11 @@ public final class Settings {
         public static final String LOCKSCREEN_BATTERY_INFO = "lockscreen_battery_info";
 
         /**
+         * @hide
+         */
+        public static final String UDFPS_ICON = "udfps_icon";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
