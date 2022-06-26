e7690922dd76c593c6fd8daac222eb6a420ae1fc
cjh1249131356 <cjh1249131356@gmail.com>
2022-06-26 22:19:19 +0800

    SystemUI: Relax slider haptic feedback
    - Increase interval.
    - Reduce duration range to 1 ~ 49ms.
    - Add check to avoid 100ms haptic on first touch.
    
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -175,7 +175,7 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
     private static final int DRAWER_ANIMATION_DURATION_SHORT = 175;
     private static final int DRAWER_ANIMATION_DURATION = 250;
 
-    private static final int HAPTIC_MIN_GAP = 15;
+    private static final int HAPTIC_MIN_GAP = 50;
 
     /** Shows volume dialog show animation. */
     private static final String TYPE_SHOW = "show";
@@ -420,6 +420,8 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
         mVibratorHelper = new VibratorHelper(mContext, true,
                 Settings.System.HAPTIC_FEEDBACK_ENABLED,
                 Settings.System.HAPTIC_ON_VOLUME_SLIDER);
+
+        mLastHapticTimestamp = -1;
 
         initDimens();
 
@@ -2893,11 +2895,12 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
             }
 
             long now = SystemClock.uptimeMillis();
-            if (progress == 0 || progress == 3000) {
+            if ((progress == 0 || progress == 3000) && mLastHapticTimestamp != -1) {
+                mLastHapticTimestamp = now;
                 mVibratorHelper.vibrateForDuration(100);
             } else if (now - mLastHapticTimestamp > HAPTIC_MIN_GAP) {
                 mLastHapticTimestamp = now;
-                final int duration = (int) (1 + 0.026 * progress);
+                final int duration = (int) (1 + 0.016 * progress);
                 mVibratorHelper.vibrateForDuration(duration);
             }
         }
