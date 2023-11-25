7cb6d1d22f5c37454cc62befedc2771b10de4d7c
maxwen <max.weninger@gmail.com>
2023-11-25 11:24:59 +0000

    base: Add Lockscreen Weather with OmniJaws
    Co-authored-by: maxwen <max.weninger@gmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I3c94fb5d002bcc942524b3aa1e78ddf0d0ac0991
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7276,6 +7276,16 @@ public final class Settings {
         }
 
         /**
+         * @hide
+         */
+        public static final String LOCKSCREEN_WEATHER_ENABLED = "lockscreen_weather_enabled";
+
+        /**
+         * @hide
+         */
+        public static final String LOCKSCREEN_WEATHER_LOCATION = "lockscreen_weather_location";
+
+        /**
          * These are all hidden system settings.
          *
          * @hide
