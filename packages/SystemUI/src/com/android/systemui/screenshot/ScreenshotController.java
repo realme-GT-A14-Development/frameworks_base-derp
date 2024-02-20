0362b292ab5986399f810c17a46a929ce8d407fa
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-02-20 12:42:37 +0530

    fixup! SystemUI: Cleanup screenshot sound/vibration logic
    - unregister camera listener if we registered it
    - move settings check to content observer
    
    Change-Id: I0c831611f5c2c6331ca45ee5124b963f54de80b1

--- packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
@@ -52,6 +52,7 @@ import android.content.IntentFilter;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
+import android.database.ContentObserver;
 import android.graphics.Bitmap;
 import android.graphics.Insets;
 import android.graphics.Rect;
@@ -295,6 +296,7 @@ public class ScreenshotController {
     private final ListenableFuture<MediaPlayer> mCameraSound;
     private final AudioManager mAudioManager;
     private final Vibrator mVibrator;
+    private CameraManager mCameraManager = null;
     private int mCamsInUse = 0;
     private final ScrollCaptureClient mScrollCaptureClient;
     private final PhoneWindow mWindow;
@@ -348,6 +350,17 @@ public class ScreenshotController {
         @Override
         public void onCameraClosed(String cameraId) {
             mCamsInUse--;
+        }
+    };
+
+    private boolean mScreenshotSoundEnabled;
+
+    private final ContentObserver mSettingObserver = new ContentObserver(
+            new Handler(Looper.getMainLooper())) {
+        @Override
+        public void onChange(boolean selfChange) {
+            mScreenshotSoundEnabled = Settings.System.getIntForUser(mContext.getContentResolver(),
+                    Settings.System.SCREENSHOT_SHUTTER_SOUND, 1, UserHandle.USER_CURRENT) == 1;
         }
     };
 
@@ -464,7 +477,8 @@ public class ScreenshotController {
         if (SystemProperties.getBoolean("audio.camerasound.force", false)
                 || mContext.getResources().getBoolean(
                         com.android.internal.R.bool.config_camera_sound_forced)) {
-            mContext.getSystemService(CameraManager.class).registerAvailabilityCallback(
+            mCameraManager = (CameraManager) mContext.getSystemService(Context.CAMERA_SERVICE);
+            mCameraManager.registerAvailabilityCallback(
                     mCamCallback, new Handler(Looper.getMainLooper()));
         }
 
@@ -479,6 +493,10 @@ public class ScreenshotController {
         mContext.registerReceiver(mCopyBroadcastReceiver, new IntentFilter(
                         ClipboardOverlayController.COPY_OVERLAY_ACTION),
                 ClipboardOverlayController.SELF_PERMISSION, null, Context.RECEIVER_NOT_EXPORTED);
+
+        mContext.getContentResolver().registerContentObserver(Settings.System.getUriFor(
+                Settings.System.SCREENSHOT_SHUTTER_SOUND), false, mSettingObserver);
+        mSettingObserver.onChange(false);
 
         // Grab PackageManager
         mPm = mContext.getPackageManager();
@@ -654,6 +672,9 @@ public class ScreenshotController {
         releaseMediaPlayer();
         releaseContext();
         TaskStackChangeListeners.getInstance().unregisterTaskStackListener(mTaskListener);
+        if (mCameraManager != null) {
+            mCameraManager.unregisterAvailabilityCallback(mCamCallback);
+        }
         mBgExecutor.shutdownNow();
     }
 
@@ -662,6 +683,7 @@ public class ScreenshotController {
      */
     private void releaseContext() {
         mContext.unregisterReceiver(mCopyBroadcastReceiver);
+        mContext.getContentResolver().unregisterContentObserver(mSettingObserver);
         mContext.release();
     }
 
@@ -1355,8 +1377,7 @@ public class ScreenshotController {
     }
 
     private void playShutterSound() {
-        boolean playSound = Settings.System.getIntForUser(mContext.getContentResolver(),
-                Settings.System.SCREENSHOT_SHUTTER_SOUND, 1, UserHandle.USER_CURRENT) == 1
+        boolean playSound = mScreenshotSoundEnabled
                 && mAudioManager.getRingerMode() == AudioManager.RINGER_MODE_NORMAL;
         boolean playSoundForced = mCamsInUse > 0;
         if (playSoundForced || playSound) {
