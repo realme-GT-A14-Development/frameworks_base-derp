3347509309f83d412190b37b958d16d72ca2f04e
cjh1249131356 <cjh1249131356@gmail.com>
2024-03-02 19:56:50 +0100

    SystemUI: Use VibratorHelper for custom haptic feedback
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -3715,8 +3715,8 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
      */
     private void maybeVibrateOnOpening(boolean openingWithTouch) {
         if (mVibrateOnOpening && mBarState != KEYGUARD && mBarState != SHADE_LOCKED) {
-            if (!openingWithTouch || !mHasVibratedOnOpen && Settings.System.getInt(mView.getContext().getContentResolver(),
-                Settings.System.HAPTIC_ON_STATUSBAR, 1) != 0) {
+            if (!openingWithTouch || !mHasVibratedOnOpen && Settings.System.getIntForUser(mView.getContext().getContentResolver(),
+                Settings.System.HAPTIC_ON_STATUSBAR, 1, UserHandle.USER_CURRENT) != 0) {
                 if (mFeatureFlags.isEnabled(ONE_WAY_HAPTICS_API_MIGRATION)) {
                     mVibratorHelper.performHapticFeedback(
                             mView,
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesCommandQueueCallbacks.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesCommandQueueCallbacks.java
@@ -32,6 +32,7 @@ import android.os.SystemClock;
 import android.os.VibrationAttributes;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
+import android.os.UserHandle;
 import android.provider.Settings;
 import android.util.Log;
 import android.util.Slog;
@@ -319,8 +320,8 @@ public class CentralSurfacesCommandQueueCallbacks implements CommandQueue.Callba
         } else if (KeyEvent.KEYCODE_SYSTEM_NAVIGATION_DOWN == key.getKeyCode()) {
             mMetricsLogger.action(MetricsEvent.ACTION_SYSTEM_NAVIGATION_KEY_DOWN);
             if (mShadeViewController.isFullyCollapsed()) {
-                if (mVibrateOnOpening && Settings.System.getInt(mContext.getContentResolver(),
-                        Settings.System.HAPTIC_ON_STATUSBAR, 1) != 0) {
+                if (mVibrateOnOpening && Settings.System.getIntForUser(mContext.getContentResolver(),
+                        Settings.System.HAPTIC_ON_STATUSBAR, 1, UserHandle.USER_CURRENT) != 0) {
                     vibrateOnNavigationKeyDown();
                 }
                 mShadeViewController.expand(true /* animate */);
