yaap/frameworks_base_old

base: Add a toggle for secure tiles on keyguard [1/2]

packages/SystemUI/src/com/android/systemui/qs/tiles/SecureQSTile.kt
@@ -17,6 +17,7 @@ package com.android.systemui.qs.tiles
 
 import android.os.Handler
 import android.os.Looper
+import android.provider.Settings
 import android.view.View
 import com.android.systemui.plugins.qs.QSTile
 import com.android.systemui.qs.QSHost
@@ -42,7 +43,9 @@ internal abstract class SecureQSTile<TState : QSTile.State> protected constructo
     protected abstract fun handleClick(view: View?, keyguardShowing: Boolean)
 
     override fun handleClick(view: View?) {
-        handleClick(view, mKeyguard.isMethodSecure && mKeyguard.isShowing)
+        val enabled: Boolean = Settings.Secure.getInt(mContext.getContentResolver(),
+            Settings.Secure.QSTILE_REQUIRES_UNLOCKING, 1) == 1
+        handleClick(view, mKeyguard.isMethodSecure && mKeyguard.isShowing && enabled)
     }
 
     protected fun checkKeyguard(view: View?, keyguardShowing: Boolean): Boolean {

realme-GT-A14-Development/frameworks_base-derp

packages/SystemUI/src/com/android/systemui/qs/tiles/SecureQSTile.kt
@@ -17,6 +17,7 @@ package com.android.systemui.qs.tiles
 
 import android.os.Handler
 import android.os.Looper
+import android.provider.Settings
 import android.view.View
 import com.android.systemui.plugins.qs.QSTile
 import com.android.systemui.qs.QSHost
@@ -43,7 +44,9 @@ internal abstract class SecureQSTile<TState : QSTile.State> protected constructo
     protected abstract fun handleClick(view: View?, keyguardShowing: Boolean)
 
     override fun handleClick(view: View?) {
-        handleClick(view, keyguardController.isMethodSecure && keyguardController.isShowing)
+        val enabled: Boolean = Settings.Secure.getInt(mContext.getContentResolver(),
+            Settings.Secure.QSTILE_REQUIRES_UNLOCKING, 1) == 1
+        handleClick(view, keyguardController.isMethodSecure && keyguardController.isShowing && enabled)
     }
 
     protected fun checkKeyguard(view: View?, keyguardShowing: Boolean): Boolean {