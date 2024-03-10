yaap/frameworks_base

base: Add airplane mode enhancement toggle [1/2]
Only here for backup

Change-Id: Ibc854943572d438b7b94ead3b02a22d94794dce9

packages/SettingsProvider/src/android/provider/settings/backup/GlobalSettings.java
@@ -105,5 +105,6 @@ public class GlobalSettings {
         Settings.Global.HDR_FORCE_CONVERSION_TYPE,
         Settings.Global.Wearable.RTL_SWIPE_TO_DISMISS_ENABLED_DEV,
         Settings.Global.Wearable.REDUCE_MOTION,
+        Settings.Global.APM_ENHANCEMENT,
     };
 }

realme-GT-A14-Development/frameworks_base-derp

packages/SettingsProvider/src/android/provider/settings/backup/GlobalSettings.java
@@ -110,6 +110,7 @@ public class GlobalSettings {
         Settings.Global.WAKELOCK_BLOCKING_ENABLED,
         Settings.Global.WAKELOCK_BLOCKING_LIST,
         Settings.Global.ALARM_BLOCKING_ENABLED,
-        Settings.Global.ALARM_BLOCKING_LIST
+        Settings.Global.ALARM_BLOCKING_LIST,
+        Settings.Global.APM_ENHANCEMENT
     };
 }
e30f131d4e15bdcc683ad33098aef11b080d79ea
Ido Ben-Hur <idoybh2@gmail.com>
2023-12-14 12:56:11 -0500

    base: Add airplane mode enhancement toggle [1/2]
    Only here for backup
    
    Change-Id: Ibc854943572d438b7b94ead3b02a22d94794dce9

--- packages/SettingsProvider/src/android/provider/settings/backup/GlobalSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/GlobalSettings.java
@@ -110,6 +110,7 @@ public class GlobalSettings {
         Settings.Global.WAKELOCK_BLOCKING_ENABLED,
         Settings.Global.WAKELOCK_BLOCKING_LIST,
         Settings.Global.ALARM_BLOCKING_ENABLED,
-        Settings.Global.ALARM_BLOCKING_LIST
+        Settings.Global.ALARM_BLOCKING_LIST,
+        Settings.Global.APM_ENHANCEMENT
     };
 }
