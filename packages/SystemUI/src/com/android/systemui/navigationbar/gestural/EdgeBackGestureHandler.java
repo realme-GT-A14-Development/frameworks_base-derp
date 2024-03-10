pixeldust-project-caf/android_frameworks_base

Gestures nav: Add Power menu action
Signed-off-by: spezi77 <spezi7713@gmx.net>
Change-Id: I11cbb003a03b17a981d957cf25aa28138426802d

packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
@@ -1035,6 +1035,9 @@ public class EdgeBackGestureHandler extends CurrentUserTracker
             case 15: // Previous song
                 PixeldustUtils.sendSystemKeyToStatusBar(KeyEvent.KEYCODE_MEDIA_PREVIOUS);
                 break;
+            case 16: // Powermenu
+                PixeldustUtils.showPowerMenu();
+                break;
         }
     }
 

realme-GT-A14-Development/frameworks_base-derp

packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
@@ -1367,6 +1367,9 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
             case 13: // Switch recent app
                 derpUtils.switchToLastApp(mContext);
                 break;
+            case 14: // Powermenu
+                derpUtils.showPowerMenu();
+                break;
         }
     }
 