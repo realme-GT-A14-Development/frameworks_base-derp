0638413d741e2d7a3ea86f6ab8e074b2480b18c9
NurKeinNeid <mralexman3000@gmail.com>
2024-04-03 02:03:58 +0200

    Revert "QS: Add haptic feedback on click"
    This reverts commit c83d7469f90682036fd80c3a52bc6771c0afed29.

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileImpl.java
@@ -38,8 +38,6 @@ import android.metrics.LogMaker;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
-import android.os.VibrationEffect;
-import android.os.Vibrator;
 import android.text.format.DateUtils;
 import android.util.ArraySet;
 import android.util.Log;
@@ -156,8 +154,6 @@ public abstract class QSTileImpl<TState extends State> implements QSTile, Lifecy
      */
     abstract protected void handleUpdateState(TState state, Object arg);
 
-    protected Vibrator mVibrator;
-
     /**
      * Declare the category of this tile.
      *
@@ -207,8 +203,6 @@ public abstract class QSTileImpl<TState extends State> implements QSTile, Lifecy
 
         resetStates();
         mUiHandler.post(() -> mLifecycle.setCurrentState(CREATED));
-
-        mVibrator = (Vibrator) mContext.getSystemService(Context.VIBRATOR_SERVICE);
     }
 
     protected final void resetStates() {
@@ -301,7 +295,6 @@ public abstract class QSTileImpl<TState extends State> implements QSTile, Lifecy
         if (!mFalsingManager.isFalseTap(FalsingManager.LOW_PENALTY)) {
             mHandler.obtainMessage(H.CLICK, eventId, 0, view).sendToTarget();
         }
-	mVibrator.vibrate(VibrationEffect.get(VibrationEffect.EFFECT_CLICK));
     }
 
     public void secondaryClick(@Nullable View view) {
