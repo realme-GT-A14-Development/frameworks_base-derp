AOSPA/android_frameworks_base-old

core: BatteryManager: Add few more Battery broadcast intents
 * current_capacity
 * design_capacity

Change-Id: Idfa9207cc8bd35469876f3094f3bc948741c94bf
Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>

core/java/android/os/BatteryManager.java
@@ -159,6 +159,20 @@ public class BatteryManager {
     public static final String EXTRA_CHARGING_STATUS = "android.os.extra.CHARGING_STATUS";
 
     /**
+     * Extra for {@link android.content.Intent#ACTION_BATTERY_CHANGED}:
+     * Int value representing the current battery capacity in microampere-hours.
+     * {@hide}
+     */
+    public static final String EXTRA_CURRENT_CAPACITY = "android.os.extra.CURRENT_CAPACITY";
+
+    /**
+     * Extra for {@link android.content.Intent#ACTION_BATTERY_CHANGED}:
+     * Int value representing the design capacity of the battery in microampere-hours.
+     * {@hide}
+     */
+    public static final String EXTRA_DESIGN_CAPACITY = "android.os.extra.DESIGN_CAPACITY";
+
+    /**
      * Extra for {@link android.content.Intent#ACTION_BATTERY_LEVEL_CHANGED}:
      * Contains list of Bundles representing battery events
      * @hide

realme-GT-A14-Development/frameworks_base-derp

 * maximum_capacity
Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

core/java/android/os/BatteryManager.java
@@ -164,6 +164,20 @@ public class BatteryManager {
     public static final String EXTRA_CHARGING_STATUS = "android.os.extra.CHARGING_STATUS";
 
     /**
+     * Extra for {@link android.content.Intent#ACTION_BATTERY_CHANGED}:
+     * Int value representing the estimated battery full charge capacity in microampere-hours.
+     * {@hide}
+     */
+    public static final String EXTRA_MAXIMUM_CAPACITY = "android.os.extra.MAXIMUM_CAPACITY";
+
+    /**
+     * Extra for {@link android.content.Intent#ACTION_BATTERY_CHANGED}:
+     * Int value representing the battery full charge design capacity in microampere-hours.
+     * {@hide}
+     */
+    public static final String EXTRA_DESIGN_CAPACITY = "android.os.extra.DESIGN_CAPACITY";
+
+    /**
      * Extra for {@link android.content.Intent#ACTION_BATTERY_LEVEL_CHANGED}:
      * Contains list of Bundles representing battery events
      * @hide