LMODroid/platform_frameworks_base

Settings: Add back option to pass swipe-up-to-unlock
This got removed unintentionally by:
  Author: Sam Mortimer <sam@mortimer.me.uk>
  Date:   Wed Oct 11 23:00:18 2017 -0700
      Remove ThirdPartyKeyguard
      Change-Id: Ie3717b9ea7a23988ebd646ed7e2fe8f105a4a693

Add back the lock util methods from the original commit:
  Author: Danesh M <daneshm90@gmail.com>
  Date:   Mon Feb 8 14:57:31 2016 -0800
      CMSettings : Add option to pass swipe-up-to-unlock (3/3)
      CYNGNOS-1873
      Change-Id: Icdd2505dbc56a77f49a76ea61daf75c5d8a24fd6

Change-Id: I6177700be955f42505dbd5fbb23461f8e151e1b4

core/java/com/android/internal/util/libremobileos/LineageLockPatternUtils.java
@@ -1,0 +1,35 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.internal.util.libremobileos;
+
+import android.content.Context;
+import android.provider.Settings;
+
+import com.android.internal.widget.LockPatternUtils;
+
+public class LineageLockPatternUtils extends LockPatternUtils {
+    public LineageLockPatternUtils(Context context) {
+        super(context);
+    }
+
+    public boolean shouldPassToSecurityView(int userId) {
+        return getBoolean(Settings.Secure.LOCK_PASS_TO_SECURITY_VIEW, false, userId);
+    }
+
+    public void setPassToSecurityView(boolean enabled, int userId) {
+        setBoolean(Settings.Secure.LOCK_PASS_TO_SECURITY_VIEW, enabled, userId);
+    }
+}

realme-GT-A14-Development/frameworks_base-derp

core/java/com/android/internal/util/derp/LineageLockPatternUtils.java
@@ -1,0 +1,35 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.internal.util.derp;
+
+import android.content.Context;
+import android.provider.Settings;
+
+import com.android.internal.widget.LockPatternUtils;
+
+public class LineageLockPatternUtils extends LockPatternUtils {
+    public LineageLockPatternUtils(Context context) {
+        super(context);
+    }
+
+    public boolean shouldPassToSecurityView(int userId) {
+        return getBoolean(Settings.Secure.LOCK_PASS_TO_SECURITY_VIEW, false, userId);
+    }
+
+    public void setPassToSecurityView(boolean enabled, int userId) {
+        setBoolean(Settings.Secure.LOCK_PASS_TO_SECURITY_VIEW, enabled, userId);
+    }
+}